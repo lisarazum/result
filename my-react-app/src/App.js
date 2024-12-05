bff51fa6418cdcf393f01956fd25d3b3a4857425
e.yashina <e.yashina@articul.ru>
2024-12-05 13:54:34 +0300

    hw 1.2

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,28 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 export const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				{/* декларативный  */}
-				<CurrentYear />
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement(
+				'img',
+				{src: logo, className: 'App-logo', alt: 'logo'}
+			),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement(
+					'code',
+					null,
+					'src/App.js',
+				),
+				' and save to reload.'
+			),
+			React.createElement(
+				'a',
+				{ className: 'App-link', href: 'https://reactjs.org', target: "_blank", rel: "noopener noreferrer"},
+				'Learn React',
+			),
+			React.createElement(
+				'p',
+				null,
+				`Текущий год: ${CurrentYear()}`
+			)
+		)
 	);
-};
+}
 
 function CurrentYear() {
 	//императивный
